22db1eb9bc803a143b89fa70255ca11ea4a9fd16
CaptainBrando! <82909107+cptnbrando@users.noreply.github.com>
2026-04-16 12:35:17 -0500

    List COM Objects

--- scripts/sys-info/list-all.ps1
+++ scripts/sys-info/list-all.ps1
@@ -1,6 +1,7 @@
 . ./list-drivers.ps1
 . ./list-programs.ps1
 . ./list-appx.ps1
+. ./list-com.ps1
 . ./list-services.ps1
 . ./list-tasks.ps1
 . ./list-users.ps1
---
+++ scripts/sys-info/list-com.ps1
@@ -0,0 +1,10 @@
+# List COM objects from the registry and export to CSV. COM Objects are like app dependencies. They can be used by multiple applications, and if they are vulnerable, they can be exploited by any application that uses them. If anything bad gets installed here, anything can use it.
+
+$ExportPath = ".\Sys-Com.csv"
+
+Get-ChildItem HKLM:\Software\Classes -ErrorAction SilentlyContinue | 
+Where-Object { $_.PSChildName -match '^\w+\.\w+$' } | 
+Select-Object PSChildName |
+Export-Csv -Path $ExportPath -NoTypeInformation
+
+Write-Host "COM Object audit exported to $ExportPath" -ForegroundColor Cyan
