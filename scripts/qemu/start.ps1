6d35b20c185f1df6210cf3c6a2538372b11ab268
CaptainBrando! <82909107+cptnbrando@users.noreply.github.com>
2026-04-16 20:38:39 -0500

    QEMU and UEFI Variables

--- scripts/qemu/start.ps1
+++ scripts/qemu/start.ps1
@@ -1,14 +1,29 @@
-qemu-system-x86_64.exe ^
--accel whpx, kernel-irqchip=on ^
--cpu host ^
--m 8G ^
--smp 4 ^
--drive if=pflash, format=raw, readonly=on, file="C:\path\to\OVMF_CODE.fd" ^
--drive if=pflash, format=raw, file="C:\path\to\OVMF_VARS.fd" ^
--device virtio-vga-gl ^
--display sdl, gl=on ^
--device virtio-blk-pci, drive=drive0 ^
--drive file=win11_vm.qcow2, format=qcow2, if=none, id=drive0 ^
--device usb-tablet ^
--cdrom "C:\path\to\Win11_English_x64.iso" ^
--drive file="C:\path\to\virtio-win.iso", media=cdrom
+# This should yield a quick way to test answerfiles and scripts on fresh installs
+# Install QEMU https://www.qemu.org/
+
+$QEMU_PATH = "C:\Program Files\qemu"
+$VM_DIR = "C:\your\vm\folder"
+$ISO_PATH = "C:\path\to\Win11.iso"
+
+# This folder will act as a USB injected into qemu at launch. It should hold an autounattend.xml file
+$ANSWERFILE_RELATIVE = "../../win-install"
+$ANSWERFILE_DIR = (Get-Item $ANSWERFILE_RELATIVE).FullName
+
+# Ensure the VARS file exists in the VM dir (copy from source if missing)
+if (!(Test-Path "$VM_DIR\OVMF_VARS.fd")) {
+    Copy-Item "$QEMU_PATH\OVMF_VARS.fd" "$VM_DIR\OVMF_VARS.fd"
+}
+
+# Optional: Reset the virtual disk to a clean state if you have a base image
+# Copy-Item "$VM_DIR\win11_clean.qcow2" "$VM_DIR\win11.qcow2" -Force
+
+# Start QEMU with WHPX and NVMe for max speed
+& "$QEMU_PATH\qemu-system-x86_64.exe" `
+-m 8G -smp 4 -accel whpx,kernel-irqchip=on -cpu host `
+-drive if=pflash,format=raw,readonly=on,file="$QEMU_PATH\OVMF_CODE.fd" `
+-drive if=pflash,format=raw,file="$VM_DIR\OVMF_VARS.fd" `
+-device virtio-vga-gl -display sdl,gl=on `
+-drive file="$VM_DIR\win11.qcow2",if=nvme,discard=on `
+-cdrom $ISO_PATH `
+-drive file=fat:rw:$ANSWERFILE_DIR,format=raw `
+-device usb-tablet -net nic,model=virtio -net user
---
+++ scripts/sys-info/list-UEFIVars.ps1
@@ -0,0 +1,2 @@
+# This uses bcdedit to bridge the gap between Windows and UEFI VARS
+bcdedit /enum firmware
