dcfc21f3d0f715322e7632add0d6e7642f882847
CaptainBrando! <82909107+cptnbrando@users.noreply.github.com>
2026-04-20 01:29:17 -0500

    Better warning and colors

--- scripts/sys-info/list/utils.ps1
+++ scripts/sys-info/list/utils.ps1
@@ -24,6 +24,12 @@ function Initialize-AuditFile {
 
     # Handle Archiving (clears old runs of this specific module)
     Get-ChildItem -Path $DataFolder -Filter "Sys-$Name-*.csv" | Where-Object { $_.Name -ne $FileName } | ForEach-Object {
+        # Visual Output
+        Write-Host "[!] " -ForegroundColor DarkGray -NoNewline
+        Write-Host "$Name " -ForegroundColor DarkGreen -NoNewline
+        Write-Host "Archiving old csv file. Destination: $(Join-Path $BackupFolder $_.Name)" -ForegroundColor DarkGray
+    
+        # Logic (Now on a new line, so it won't print)
         Move-Item -Path $_.FullName -Destination (Join-Path $BackupFolder $_.Name) -Force
     }
 
